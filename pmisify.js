3989c20a559cc2f6494de699ff7d901b6aca8747
jryzj <2381002887@qq.com>
2020-07-31 16:26:04 +0800

    add comments

--- pmisify.js
+++ pmisify.js
@@ -1,3 +1,8 @@
+//use to promisify async function.
+//the last parameter of async function is a callback
+//after promisified, the async function will be sync.
+//how to use, see demo.js
+
 function pmsify(fun) {
   return function (...arg) {
     return new Promise((resolve, reject) => {
